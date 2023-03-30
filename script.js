67af72e8cb69251b9ea5279d0a5b02366dbe8c65
Sharan <agnimurli@gmail.com>
2023-03-30 18:50:26 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.nav-icon1').toggleClass('open');
+    });
+
+
+    $('.about-nxt-button').on('click', function () {
+  
+      $('.about-page-content').hide().fadeOut(500),1000;
+      $('.about-page-content1').show().fadeIn(500),1000;
+
+    });
+
+    $('.about-nxt-button1').on('click', function () {
+  
+      $('.about-page-content1').hide().fadeOut(500),1000;
+      $('.about-page-content2').show().fadeIn(500),1000;
+
+    });
+
+    $('.about-nxt-button2').on('click', function () {
+  
+      $('.about-page-content2').hide().fadeOut(500),1000;
+      $('.about-page-content').show().fadeIn(500),1000;
+
+    });
+   
+  });
+
+  $(document).ready(function () {
+  
+      $(".about-page-content").hide();
+      $(".about-page-content1").hide();
+      $(".about-page-content2").hide();
+
+
+  
+  });
+
+
+  $(function() {
+    setTimeout(function() { $("#about-hide").fadeOut(1500); 
+    $(".about-page-content").fadeIn(1900)}, 1000)
+    
+    
+    })
+
+
+    
